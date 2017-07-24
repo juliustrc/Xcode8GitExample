c90acb327a4ffec8fb9e973be967da37736d1327
Julio Ornelas <julio@Julios-MacBook-Air.local>
2017-07-24 17:05:04 -0500

    Initial Commit

---
+++ XCode8GitExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCode8GitExample
+//
+//  Created by Julio Ornelas on 7/24/17.
+//  Copyright © 2017 Julio Ornelas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
